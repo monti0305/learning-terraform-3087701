c764571adcf98a2a0953e3c5c78ea812f7b12c0b
monti0305 <172427493+monti0305@users.noreply.github.com>
2025-09-29 14:05:04 -0400

    fixed error

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.vpc_security_group_ids]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
 
@@ -51,7 +51,7 @@ source = "terraform-aws-modules/security-group/aws"
 version = "4.13.0"
 
 vpc_id = module.blog_vpc.vpc_id
-name=blog
+name   = "blog"
 ingress_rules = ["https-443-tcp","http-80-tcp" ]
 ingress_cidr_blocks = ["0.0.0.0/0"]
 egress_rules = ["all-all"]
