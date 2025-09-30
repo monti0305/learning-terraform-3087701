a1787b450597a2a733c979f075ef9205aec1ba26
monti0305 <172427493+monti0305@users.noreply.github.com>
2025-09-30 13:57:02 -0400

    try again

--- main.tf
+++ main.tf
@@ -11,8 +11,9 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = [var.ami_filter.owner]
+  owners = [var.ami_filter.owner] # Bitnami
 }
+
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -20,8 +21,9 @@ module "blog_vpc" {
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
+
 
   tags = {
     Terraform = "true"
@@ -29,21 +31,23 @@ module "blog_vpc" {
   }
 }
 
-module "blog_sg" {
 
-source = "terraform-aws-modules/security-group/aws"
-version = "4.13.0"
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-vpc_id = module.blog_vpc.vpc_id
-name   = "blog"
-ingress_rules = ["https-443-tcp","http-80-tcp" ]
-ingress_cidr_blocks = ["0.0.0.0/0"]
-egress_rules = ["all-all"]
-egress_cidr_blocks = ["0.0.0.0/0"]
+  name = "blog"
+
+  min_size            = var.asg_min
+  max_size            = var.asg_max
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
-
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
@@ -73,22 +77,18 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = var.environment.name
+    Environment = "dev"
   }
 }
 
-module "blog_autoscaling" {
-  
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
 
-  name = "blog"
-
-  min_size            = var.asg_min
-  max_size            = var.asg_max
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
